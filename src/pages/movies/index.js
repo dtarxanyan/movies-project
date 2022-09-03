a9f40cc400f680354f1c389ad5f956c8f9f18be3
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-09-03 22:48:22 +0400

    Fixed rows and search

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,8 +1,49 @@
 import {getMovies} from '../../api/movie.js'
 import getMoviesListHTML from '../../renders/getMoviesListHTML.js'
 
-getMovies().then((movies) => {
+const searchBtn = document.querySelector('#searchBtn')
+const searchInput = document.querySelector('#searchInput')
+
+searchBtn.addEventListener('click', () => {
+    const container = document.getElementById('movie-container');
+    if(searchInput.value) {
+       getMovies().then((movies) => {
+       let searched = movies.filter(el => String(el.name).toLowerCase().includes(searchInput.value.toLowerCase()))
+
+       if(emptySearchResult(searched,container)) return 
+       
+       const mooviesHTML = getMoviesListHTML(searched)
+       container.innerHTML = ''
+       container.append(mooviesHTML)
+       }) 
+    } else {
+        container.innerHTML = ''
+        draw()
+       }
+})
+
+ function draw() { 
+    getMovies().then((movies) => {
     const mooviesHTML = getMoviesListHTML(movies);
     const container = document.getElementById('movie-container');
-    container.innerHTML = mooviesHTML;
+    container.append(mooviesHTML)
 })
+}
+
+draw()
+
+
+function emptySearchResult(searched,container) {
+    if(!searched.length) {
+        const notFound = document.createElement('div')
+        notFound.innerText = 'Nothing found'
+        notFound.classList.add('notFound')
+        container.innerHTML= ''
+        container.append(notFound)
+        return 1
+       }
+    return 0
+}
+
+
+
