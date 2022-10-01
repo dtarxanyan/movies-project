ded22b8642a99dadf3a126fabe5e357b0ba8195c
aodavittar <davitt@kyocare.com>
2022-10-01 16:36:56 +0400

    Create text input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Sidebar from "./components/SideBar/Sidebar";
+import TextInput from "./components/TextInput/TextInput";
 
 class App extends React.Component {
     render() {
         return (
-            <Sidebar />
+            <TextInput
+                label={'First Name'}
+                placeholder={'Enter Your Name'}
+                value={'Initial Value'}
+                onChange={(newValue) => {
+                    console.log(newValue)
+                }}
+            />
         )
     }
 }
