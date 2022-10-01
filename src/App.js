bdcb466d6015e6981d95a18547bf6c4e5ccd0270
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-10-01 17:39:06 +0400

    Dropdown created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React from "react";
 import TextInput from "./components/TextInput/TextInput";
+import Select from "./components/Select/Select";
 
 class App extends React.Component {
-    render() {
-        return (
-            <TextInput
-                label={'First Name'}
-                placeholder={'Enter Your Name'}
-                value={'Initial Value'}
-                onChange={(newValue) => {
-                    console.log(newValue)
-                }}
-            />
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      options: [],
+    };
+  }
+
+  async componentDidMount() {
+    let data = await fetch("/data/sidebar.json");
+    let options = await data.json();
+    this.setState({ options: [...options] });
+  }
+  render() {
+    return (
+      this.state.options.length && (
+        <Select
+          options={this.state.options}
+          label="Test Label"
+          placeholder="Test Placeholder"
+          value="Test Value"
+          onChange={(value) => {
+            console.log(value);
+          }}
+        ></Select>
+      )
+    );
+  }
 }
 
 export default App;
