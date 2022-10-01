2cfea0f682768b02e7c76838c1b38874e55c192e
Suren <sur_zaqaryan@mail.ru>
2022-10-01 14:45:06 +0400

    start-react-suren-zaqaryan-input-text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Sidebar from "./components/SideBar/Sidebar";
+import Sidebar from './components/SideBar/Sidebar';
+import Input from './components/InputText/Input';
 
 class App extends React.Component {
-    render() {
-        return (
-            <Sidebar />
-        )
-    }
+  render() {
+    return <Input />;
+  }
 }
 
 export default App;
---
+++ src/components/InputText/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Input extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default Input;
