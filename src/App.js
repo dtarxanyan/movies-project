d4375d769cf8ee24ffcc0325db14d19d557995fa
sergkarapetian <sergey.karapetyan.1997@mail.ru>
2022-10-01 15:23:42 +0400

    Input with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import UserList from "./components/UserList/UserList";
+import React from "react";
+// import UserList from "./components/UserList/UserList";
+import Input from "./components/Input";
 
 class App extends React.Component {
-    render() {
-        return (
-            <UserList/>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: "Label-1",
+      placeholder: "Input your text",
+      value: "",
+      id: 1,
+      change: "foo"
+    };
+  }
+
+  handleValue = (event) => {
+    if (event) {
+      this.setState({ value: event.target.value });
     }
+  };
+
+  render() {
+    const { label, placeholder, value, id, change } = this.state;
+    return (
+      <Input
+        label={label}
+        placeholder={placeholder}
+        value={value ? value : ""}
+        id={id}
+        change = {this.handleValue}
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Input/index.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+export default class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "input your value"
+    };
+  }
+
+  handleInput = () => {
+    this.setState({
+      value: "value was changed"
+    });
+  };
+
+  handleReset=()=>{}
+
+  render() {
+    const {placeholder, value, id, change} = this.props
+    return (
+      <div>
+            <label htmlFor={id? id: null}>1. </label>
+          <input placeholder = {placeholder} value={value} id = {id} onChange = {change}></input>
+       
+      </div>
+    );
+  }
+}
