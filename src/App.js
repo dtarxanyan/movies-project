6d0cc328b2842d5c3c88543d62393f70b3bdcdb8
DavIdHabeTyan <davidhabetyanm@gmail.com>
2022-10-01 14:59:32 +0400

    changed code

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import InputBar from "./components/inputBar";
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.state={
+            inputValue: "",
+        }
+    }
 
     handelInput = (e) => {
         const value = e.target.value
-
+        this.setState({
+            inputValue: value,
+        })
     }
 
-
     render() {
+        const {value} = this.state;
         return (
             <>
             <Sidebar />
+                <h2>{value}</h2>
             <InputBar handelInput ={this.handelInput}/>
             </>
         )
     }
 }
-
 export default App;
--- src/components/inputBar/index.js
+++ src/components/inputBar/index.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class InputBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         const {handelInput} = this.props;
         return (
