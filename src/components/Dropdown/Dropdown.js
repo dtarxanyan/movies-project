ee7bebadad8242fe3e195395719c0a0e35997112
ArturHovakimyan <welcomearturl@gmail.com>
2022-10-08 14:43:17 +0400

    not corect

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Dropdown (){
+    return(
+        <select>
+            <option value="grapefruit" >Grapefruit</option>
+            <option value="lime" >Lime</option>
+            <option selected value="coconut" >Coconut</option>
+            <option value="mango" >Mango</option>
+        </select>
+        
+    )
+}
+
+export default Dropdown
