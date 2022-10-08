d463eb7e28eee98bf9c55d2b67e40374c445132d
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-10-08 12:58:33 +0400

    Created dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import TableContainer from "./components/Table/TableContainer";
+import TextInput from "./components/TextInput/TextInput";
+import Table from './components/Table/Table';
+import Dropdown from './components/Dropdown/Dropdown'
 
 class App extends React.Component {
-    render() {
-        return (
-            <TableContainer/>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      options: [],
+    };
+  }
+  async componentDidMount() {
+    let data = await fetch("/data/sidebar.json");
+    let options = await data.json();
+    this.setState({ options: [...options] });
+  }
+  render() {
+    return (
+      this.state.options.length && (
+        <Dropdown
+          options={this.state.options}
+          label="Test Label"
+          value="Test Value"
+          onChange={(value) => {
+            console.log(value);
+          }}
+        ></Dropdown>
+      )
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./css/dropdown.css";
+
+export default class Select extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ value });
+    this.props.onChange(value);
+  };
+  render() {
+    const { label, value, options } = this.props;
+    return (
+      <div className="selectContainer">
+        <label htmlFor="select">{label}</label>
+        <select
+          value={this.state.value}
+          name="select"
+          id="select"
+          onChange={(e) => {
+            this.handleChange(e);
+          }}
+        >
+          <option>{value}</option>
+          {options.map((el) => {
+            return (
+              <option key={el.id} value={el.name}>
+                {el.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  }
+}
