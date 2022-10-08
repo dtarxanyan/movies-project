23f28d49f10dcafee1988ebb2787a835a88f40d5
Suren <sur_zaqaryan@mail.ru>
2022-10-08 16:13:22 +0400

    suren-zaqaryan-border-task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import TableContainer from './components/Table/TableContainer';
 import DropdownContainer from './components/Dropdown/DropdownContainer';
+import './App.css';
+
+class Border extends React.Component {
+  render() {
+    const { children } = this.props;
+    return <div className="border">{children}</div>;
+  }
+}
 
 class App extends React.Component {
   render() {
-    return <DropdownContainer />;
+    return <Border>Armenia</Border>;
   }
 }
 
