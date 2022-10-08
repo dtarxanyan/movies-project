788fb233e2b7b668618c926d1fe9444609d19619
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-10-08 16:10:20 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TableContainer from './components/Table/TableContainer';
 import DropdownContainer from './components/Dropdown/DropdownContainer';
+import Border from './components/Border/Border';
 
 class App extends React.Component {
   render() {
-    return <DropdownContainer />;
+    return <Border>Rapapapapam rapapampampam </Border>;
   }
 }
 
---
+++ src/components/Border/Border.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./css/border.css"
+
+export default class Border  extends Component {
+  render() {
+    return (
+      <div className='bordered'>
+        {this.props.children}
+      </div>
+    )
+  }
+}
