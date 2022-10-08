12df2b2e8484b0914058a3f2ecb946ae96bcbc1e
ArturHovakimyan <welcomearturl@gmail.com>
2022-10-08 16:33:03 +0400

    formating code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import Frame from './components/Frame/Frame';
+import React from "react";
+import Frame from "./components/Frame/Frame";
 import TextInput from "./components/TextInput/TextInput";
 
 class App extends React.Component {
-    render() {
-        return (<>
-            <TextInput
-                label={'First Name'}
-                placeholder={'Enter Your Name'}
-                value={'Initial Value'}
-                onChange={(newValue) => {
-                    console.log(newValue)
-                }}
-            />
-            <Frame>
-                <p>chaild</p>
-            </ Frame>
-            </> )
-    }
+  render() {
+    return (
+      <>
+        <TextInput
+          label={"First Name"}
+          placeholder={"Enter Your Name"}
+          value={"Initial Value"}
+          onChange={(newValue) => {
+            console.log(newValue);
+          }}
+        />
+        <Frame>
+          <p>chaild</p>
+        </Frame>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-
-class Frame extends React.Component{
-    render(){
-        const { children } = this.props
-        return(
-            <div className="frame" >
-               {children} 
-            </div>
-            
-        )
-    }
+class Frame extends React.Component {
+  render() {
+    const { children } = this.props;
+    return <div className="frame">{children}</div>;
+  }
 }
 
-export default Frame
+export default Frame;
