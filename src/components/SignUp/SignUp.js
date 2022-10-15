e8b47248733645969ac1f5f76bde4ae7635c8b2b
Dav98 <davitkirakosyan99@gmail.com>
2022-10-15 15:41:37 +0400

    input_value

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -20,28 +20,24 @@ class SignUp extends Component {
 
         }
     }
-    handleInput(name,value){
+    handleInput=(name,value)=>{
         this.setState({value:{
-            ...this.state.value,
+            ...this.state.values,
                 [name]:value
         }})
     }
 
-    // handleChange = (e) => {
-    //     const value = e.target.value;
-    //     this.setState({value});
-    //     this.props.onChange(value);
-    // }
+
 
     onSubmit =()=>{
-        this.props.onSubmit(this.state.value)
+        this.props.onSubmit(this.state.values)
     }
 
 
 
 
     render() {
-        const {firstName,lastName,password,email,handleInput} = this.state.values
+        const {firstName,lastName,password,email} = this.state.values
 
         return (
             <div>
@@ -49,11 +45,11 @@ class SignUp extends Component {
                 <form action="">
                     <div className={'firstname-lastname'}>
                         <TextInput onChange={(e)=>this.handleInput(firstName,e)}  value={ firstName } placeholder={'First name'}/>
-                        <TextInput  onChange={(e)=>handleInput(lastName,e)} value = { lastName } placeholder={'Last name'}/>
+                        <TextInput  onChange={(e)=>this.handleInput(lastName,e)} value = { lastName } placeholder={'Last name'}/>
                     </div>
                     <br/>
-                    <TextInput onChange={(e)=>handleInput(email,e)}  value ={ email } placeholder={'Mobile Number or email'}/>
-                    <TextInput onChange={(e)=>handleInput(password,e)} value = { password } placeholder={'New password'}/>
+                    <TextInput onChange={(e)=>this.handleInput(email,e)}  value ={ email } placeholder={'Mobile Number or email'}/>
+                    <TextInput onChange={(e)=>this.handleInput(password,e)} value = { password } placeholder={'New password'}/>
                     <br/>
                     <br/>
                     <BirthdayInput/>
