b6aa82174534c65c26d23f21bf59b4c60acc2695
vahagnmovs <vahag999988@gmail.com>
2022-10-15 10:46:53 +0400

    created facebook sign-up lokk like

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,4 +1,3 @@
-import { getValue } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 
 class Dropdown extends React.Component {
@@ -9,21 +8,7 @@ class Dropdown extends React.Component {
   render() {
     return (
       <>
-        <label htmlFor="names">{this.props.label}</label>
 
-        <select id="names" onChange={this.props.onChange}>
-          {this.props.options.map((option) => {
-            if (option.id === this.props.value.id) {
-              return (
-                <option selected key={option.id}>
-                  {option.firstName}
-                </option>
-              );
-            } else {
-              return <option key={option.id}>{option.firstName}</option>;
-            }
-          })}
-        </select>
       </>
     );
   }
