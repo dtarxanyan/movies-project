4bba831a6c3243b9cbb565ae7df59944243239b1
Narek-Hovakimyan <narek.hovakimyan.1999@mail.ru>
2022-10-15 15:26:54 +0400

    form-singUp

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from 'react';
 import SignUp from "./components/SignUp/SignUp";
 
 class App extends React.Component {
+
+
+  handleSubmit(values) {
+    console.log(values);
+  }
+
   render() {
-    return <SignUp />;
+    return <SignUp
+      defaultValue={{
+        fristName: 'Narek',
+        lastName: 'Hovakimyan',
+        number: '1111111',
+        password: '555555',
+        onSubmit: this.handleSubmit
+      }
+      }
+    />;
   }
 }
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,27 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import BirthdayInput from './BirthdayInput/BirthdayInput'
 import GenderInput from './GenderInput/GenderInput'
 import TextInput from '../TextInput/TextInput'
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props)
+        const { defaultValue} = props
+        this.state = {
+            values: {
+                ...defaultValue
+            }
+        }
+    }
+    onSubmit(e) {
+        console.log(e.value);
+    }
+
+
+
     render() {
+        const { fristName, lastName, number, password } = this.state.values
         return (
             <div>
-                <form action="">
+                <form action=""  >
                     <div className={'firstname-lastname'}>
-                        <TextInput placeholder={'First name'}/>
-                        <TextInput placeholder={'Last name'}/>
+                        <TextInput value={fristName} placeholder={'First name'} />
+                        <TextInput value={lastName} placeholder={'Last name'} />
                     </div>
-                    <br/>
-                    <TextInput placeholder={'Mobile Number or email'}/>
-                    <TextInput placeholder={'New password'}/>
-                    <br/>
-                    <br/>
-                    <BirthdayInput/>
-                    <br/>
-                    <GenderInput/>
-                    <br/>
-                    <button type={'submit'}>Sign Up</button>
+                    <br />
+                    <TextInput value={number} placeholder={'Mobile Number or email'} />
+                    <TextInput value={password} placeholder={'New password'} />
+                    
+                    <br />
+                    <BirthdayInput />
+                    <br />
+                    <GenderInput />
+                    <br />
+                    <button onSubmit={(value)=> this.props.onSubmit(console.log(value))}  type={'submit'}>Sign Up</button>
                 </form>
             </div>
         );
