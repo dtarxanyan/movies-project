56b37b9e59f6f0d092f5c31917ca0b2a4ae71c5c
vahagnmovs <vahag999988@gmail.com>
2022-10-15 15:14:00 +0400

    created default values for inputs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 import SignUp from "./components/SignUp/SignUp";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  handleSubmit = (value) => {
+      console.log(value)
+  }
+
   render() {
-    return <SignUp />;
+
+
+    return <SignUp
+        defaultValues = { {
+          firstName: 'Suren',
+          lastName: 'Gasparyan',
+          mobileNumber: '+37443999988',
+          _password: '********'
+        } }
+        onSubmit = { this.handleSubmit }/>;
   }
 }
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,24 +4,55 @@ import GenderInput from './GenderInput/GenderInput'
 import TextInput from '../TextInput/TextInput'
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        const { defaultValues } = this.props;
+
+        this.state = {
+            values: {
+                firstName: '',
+                lastName: '',
+                mobile: '',
+                _password: '',
+                ...defaultValues,
+            }
+        }
+
+
+    }
+
+    onSubmit = () => {
+        this.props.onSubmit(this.state.values)
+    }
+
+    handleInput = (name, value) => {
+        this.setState({ values: {
+                ...this.state.values,
+                [name]: value
+            }, })
+    }
+
     render() {
+        const { firstName, lastName, mobileNumber, _password } = this.state.values;
+
         return (
             <div>
                 <form action="">
                     <div className={'firstname-lastname'}>
-                        <TextInput placeholder={'First name'}/>
-                        <TextInput placeholder={'Last name'}/>
+                        <TextInput onChange = { (event) => this.handleInput('firstName', event) } value = { firstName } placeholder={'First name'}/>
+                        <TextInput onChange = { (event) => this.handleInput('lastName', event) } value = { lastName } placeholder={'Last name'}/>
                     </div>
                     <br/>
-                    <TextInput placeholder={'Mobile Number or email'}/>
-                    <TextInput placeholder={'New password'}/>
+                    <TextInput onChange = { (event) => this.handleInput('mobile', event) } value = { mobileNumber } placeholder={'Mobile Number or email'}/>
+                    <TextInput onChange = { (event) => this.handleInput('_password', event) } value = { _password } placeholder={'New password'}/>
                     <br/>
                     <br/>
                     <BirthdayInput/>
                     <br/>
                     <GenderInput/>
                     <br/>
-                    <button type={'submit'}>Sign Up</button>
+                    <button onClick = { this.onSubmit } type={'button'}>Sign Up</button>
                 </form>
             </div>
         );
