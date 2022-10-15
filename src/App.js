690d84f2a9e321fb257d4c58def34bd133dc37cf
AlbertGabrielyan1991 <55951023+AlbertGabrielyan1991@users.noreply.github.com>
2022-10-15 08:04:14 +0100

    albert-gabrielyan-create-form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TableContainer from './components/Table/TableContainer';
-import DropdownContainer from './components/Dropdown/DropdownContainer';
-
+// import DropdownContainer from './components/Dropdown/DropdownContainer';
+import SignUp from "./components/SignUP/SignUp";
 class App extends React.Component {
   render() {
-    return <DropdownContainer />;
+    return(
+        <SignUp/>
+    )
   }
 }
 
