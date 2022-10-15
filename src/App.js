766ba11a4edd0946cf6961424a455d9cd65f6a9f
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-10-15 15:11:40 +0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 import SignUp from "./components/SignUp/SignUp";
 
 class App extends React.Component {
-  render() {
-    return <SignUp />;
-  }
+	handleSubmit = (e, values) => {
+		e.preventDefault();
+		console.log(values);
+	};
+
+	render() {
+		return (
+			<SignUp
+				onSubmit={this.handleSubmit}
+				defaultValues={{
+					firstName: "Tiko",
+					lastName: "Mnatsakanyan",
+					mobile: "044226604",
+					password: "tutak",
+				}}
+			/>
+		);
+	}
 }
 
 export default App;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,31 +1,73 @@
-import React, {Component} from 'react';
-import BirthdayInput from './BirthdayInput/BirthdayInput'
-import GenderInput from './GenderInput/GenderInput'
-import TextInput from '../TextInput/TextInput'
+import React, { Component } from "react";
+import BirthdayInput from "./BirthdayInput/BirthdayInput";
+import GenderInput from "./GenderInput/GenderInput";
+import TextInput from "../TextInput/TextInput";
 
 class SignUp extends Component {
-    render() {
-        return (
-            <div>
-                <form action="">
-                    <div className={'firstname-lastname'}>
-                        <TextInput placeholder={'First name'}/>
-                        <TextInput placeholder={'Last name'}/>
-                    </div>
-                    <br/>
-                    <TextInput placeholder={'Mobile Number or email'}/>
-                    <TextInput placeholder={'New password'}/>
-                    <br/>
-                    <br/>
-                    <BirthdayInput/>
-                    <br/>
-                    <GenderInput/>
-                    <br/>
-                    <button type={'submit'}>Sign Up</button>
-                </form>
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+		const { firstName, lastName, mobile, password } = props.defaultValues;
+		this.state = {
+			firstName,
+			lastName,
+			mobile,
+			password,
+		};
+	}
+
+	handleChange = (value, property) => {
+		this.setState({
+			[property]: value,
+		});
+	};
+	render() {
+		const { firstName, lastName, mobile, password } = this.state;
+		return (
+			<div>
+				<form action="">
+					<div className={"firstname-lastname"}>
+						<TextInput
+							property={"firstName"}
+							onChange={this.handleChange}
+							value={firstName}
+							placeholder={"First name"}
+						/>
+						<TextInput
+							property={"lastName"}
+							onChange={this.handleChange}
+							value={lastName}
+							placeholder={"Last name"}
+						/>
+					</div>
+					<br />
+					<TextInput
+						property={"mobile"}
+						onChange={this.handleChange}
+						value={mobile}
+						placeholder={"Mobile Number or email"}
+					/>
+					<TextInput
+						property={"password"}
+						onChange={this.handleChange}
+						value={password}
+						placeholder={"New password"}
+					/>
+					<br />
+					<br />
+					<BirthdayInput />
+					<br />
+					<GenderInput />
+					<br />
+					<button
+						onClick={(e) => this.props.onSubmit(e, this.state)}
+						type={"submit"}
+					>
+						Sign Up
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignUp;
