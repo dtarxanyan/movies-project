60365197046cff1ad7b775b3309169e574c2e8e4
Tigran Petrosyan <tigranpetrosyan.707@gmail.com>
2022-10-22 00:31:39 +0400

    form-task-with-children-props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React from 'react';
-import SignUp from "./components/SignUp/SignUp";
+import React from "react";
+import Form from "./components/Form";
+import TextInput from "./components/TextInput/TextInput";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
-  handleSubmit = (value) => {
-      console.log(value)
-  }
+  handleSubmit = (values) => {
+    console.log(values);
+  };
 
   render() {
+    return (
+      <Form
+        onSubmit={this.handleSubmit}
+        defaultValues={{
+          name: "Tigran",
+          email: "Tigran@gmail.com",
+          password: "123456",
+        }}
+      >
+        {(form) => {
+          const { setValue, values } = form;
+          return (
+            <div className="container">
+              <TextInput
+                label={"Name"}
+                name={"name"}
+                value={values.name}
+                onChange={setValue}
+                placeholder="Write your name"
+              />
 
+              <TextInput
+                label={"Email"}
+                name={"email"}
+                value={values.email}
+                onChange={setValue}
+              />
 
-    return <SignUp
-        defaultValues = { {
-          firstName: 'Suren',
-          lastName: 'Gasparyan',
-          mobileNumber: '+37443999988',
-          _password: '********'
-        } }
-        onSubmit = { this.handleSubmit }/>;
+              <TextInput
+                label={"Password"}
+                name={"password"}
+                value={values.password}
+                onChange={setValue}
+              />
+
+              <button type={"submit"} className="subBTN">
+                Submit Form
+              </button>
+            </div>
+          );
+        }}
+      </Form>
+    );
   }
 }
 
