893e480724f136942875ea9a65e45f1255089f01
aodavittar <davitt@kyocare.com>
2022-10-22 12:18:16 +0400

    Create layout

--- src/App.js
+++ src/App.js
@@ -1,67 +1,19 @@
 import React from "react";
-import Form from "./components/Form";
-import TextInput from "./components/TextInput/TextInput";
-import Input from "./components/Functional/Input/Input";
+import Layout from "./components/Functional/Layout/Layout";
+import RegistrationPage from "./components/Functional/RegistrationPage/RegistrationPage";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  handleSubmit = (values) => {
-    console.log(values);
-  };
-
-  render() {
-    return (
-      <Form
-        onSubmit={this.handleSubmit}
-        defaultValues={{
-          name: "Tigran",
-          email: "Tigran@gmail.com",
-          password: "123456",
-        }}
-      >
-        {(form) => {
-          const { setValue, values } = form;
-          return (
-            <div className="container">
-              <TextInput
-                label={"Name"}
-                name={"name"}
-                value={values.name}
-                onChange={setValue}
-                placeholder="Write your name"
-              />
-
-              <TextInput
-                label={"Email"}
-                name={"email"}
-                value={values.email}
-                onChange={setValue}
-              />
-
-              <Input
-                  id={'id'}
-                  className={'class-name'}
-                  type={'password'}
-                  label={'password'}
-                  placeholder={'Enter Password...'}
-                  name={'password'}
-                  value={values.password}
-                  onChange={setValue}
-              />
-
-
-              <button type={"submit"} className="subBTN">
-                Submit Form
-              </button>
-            </div>
-          );
-        }}
-      </Form>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <RegistrationPage/>
+            </Layout>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Functional/Layout/Layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './style.css'
+
+const Layout = (props) => {
+    return (
+        <div className={'main'}>
+            <div className={'sidebar'}>
+                <ul>
+                    <li>Menu Item 1</li>
+                    <li>Menu Item 2</li>
+                    <li>Menu Item 3</li>
+                    <li>Menu Item 4</li>
+                    <li>Menu Item 5</li>
+                </ul>
+            </div>
+            <div className={'page-content'}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
