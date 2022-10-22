8c9668079fb1bd698c56407375123f6a1875ef53
Suren <sur_zaqaryan@mail.ru>
2022-10-22 12:48:34 +0400

    fix

--- src/components/Functional/RegistrationPage/RegistrationPage.js
+++ src/components/Functional/RegistrationPage/RegistrationPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const RegistrationPage = (props) => {
+const RegistrationPage = ({ disableAllInputs, allInputsColor }) => {
   return (
     <div>
       <h2>Registration Page</h2>
       <div>
         <form>
           <input
-            disabled={props.disableAllInputs}
-            style={{ color: props.allInputsColor }}
+            placeholder="..."
+            disabled={disableAllInputs}
+            style={{ color: allInputsColor }}
             type="text"
           />
         </form>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App disableAllInputs={false} allInputsColor={'blue'} />
+    <App disableAllInputs={false} allInputsColor={'red'} />
   </React.StrictMode>,
 );
 
