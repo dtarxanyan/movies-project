dfddf22823ed790605c2bffcd4576ca8ac8c4989
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2022-10-22 15:19:30 +0400

    done

--- src/components/Functional/Volume/Volume.js
+++ src/components/Functional/Volume/Volume.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Volume(props) {
     const [volume, setVolume] = useState(null)
     const [height, setHeight] = useState(null)
     
+    useEffect(() => {
+        setVolume(width * height * length)
+    },[height])
 
     const changeHeight = (e) => {
         setHeight(+e)
-        changeVolume()
     }
 
-    const changeVolume = () => {
-        setVolume(width * height * length)
-    }
 
     const {width, length} = props
   return (
     <div className='wrapper'>
         <input type="text" placeholder = "Enter a height" onChange={(e) => changeHeight(e.target.value)}></input>
         <div className='result'>
-            <span id='volumeResult'>{volume}</span>
+            <span id='volumeResult'>{volume || 0}</span>
         </div>
     </div>
   )
