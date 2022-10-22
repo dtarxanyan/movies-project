5e1207d2e266c9b15b80764e989f88a7c88357be
vahagnmovs <vahag999988@gmail.com>
2022-10-22 16:06:35 +0400

    calculate area and volume Vahagn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./components/Functional/Layout/Layout";
 import RegistrationPage from "./components/Functional/RegistrationPage/RegistrationPage";
-import CalcVolume from "./components/Functional/CalcVolume/CalcVolume";
+import CalculateAreaAndVolume from "./components/Functional/CalculateAreaAndVolume/CalculateAreaAndVolume";
 
 class App extends React.Component {
     constructor(props) {
@@ -11,7 +11,7 @@ class App extends React.Component {
     render() {
         return (
             <Layout>
-                <CalcVolume area={10}/>
+                <CalculateAreaAndVolume />
             </Layout>
         );
     }
