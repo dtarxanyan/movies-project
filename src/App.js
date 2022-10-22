efb58c75f9cb4fe61c513279f9b74b5e8a078896
Suren <sur_zaqaryan@mail.ru>
2022-10-22 14:58:29 +0400

    suren zaqaryan calc-task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import Layout from "./components/Functional/Layout/Layout";
-import RegistrationPage from "./components/Functional/RegistrationPage/RegistrationPage";
+import React from 'react';
+import Layout from './components/Functional/Layout/Layout';
+import RegistrationPage from './components/Functional/RegistrationPage/RegistrationPage';
+import Calc from './components/Calc/Calc';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Layout>
-                <RegistrationPage/>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Calc area={10} />
+      </>
+    );
+  }
 }
 
 export default App;
