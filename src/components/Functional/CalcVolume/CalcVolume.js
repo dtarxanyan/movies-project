3ec706c6d06e94c38b3724a0735330e4f722d554
vahagnmovs <vahag999988@gmail.com>
2022-10-22 16:22:33 +0400

    calculate area and volume Vahagn

--- src/components/Functional/CalcVolume/CalcVolume.js
+++ src/components/Functional/CalcVolume/CalcVolume.js
@@ -1,9 +1,15 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Input from "../Input/Input";
 
 const CalcVolume = (props) => {
     const {area} = props;
     const [volume, setVolume] = useState(0);
+    const [height, setHeight] = useState(0)
+
+
+    useEffect(() => {
+        setVolume(prev => area * height)
+    }, [area])
 
     const calcVolume = (area, height) => {
         return area * height;
@@ -12,6 +18,7 @@ const CalcVolume = (props) => {
     const onChange = (height) => {
         const vol = calcVolume(area, height);
         setVolume(vol);
+        setHeight(height)
     }
 
     return (
