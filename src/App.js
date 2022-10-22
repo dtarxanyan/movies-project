1d6140f75cbe875efde14d2e0386051a15a30a65
Tigran Petrosyan <tigranpetrosyan.707@gmail.com>
2022-10-22 12:36:52 +0400

    Tigran Petrosyan version

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Layout from "./components/Functional/Layout/Layout";
 import RegistrationPage from "./components/Functional/RegistrationPage/RegistrationPage";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Layout>
-                <RegistrationPage/>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <RegistrationPage
+          disableAllInputs={this.props.disableAllInputs}
+          inputsColor={this.props.inputsColor}
+        />
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/components/Functional/RegistrationPage/RegistrationPage.js
+++ src/components/Functional/RegistrationPage/RegistrationPage.js
@@ -1,14 +1,32 @@
 import React from "react";
 
-const RegistrationPage = () => {
-     return (
-        <div>
-            <h2>Registration Page</h2>
-            <div>
-                Registration page contents Here
+const RegistrationPage = ({ disableAllInputs, inputsColor }) => {
+  return (
+    <div>
+      <h2>Registration Page</h2>
+      <div>
+        <form autoComplete="off">
+          <div className="inputBox">
+            <input
+              disabled={disableAllInputs}
+              type="text"
+              placeholder="Name"
+              style={{ color: inputsColor }}
+            />
+            <input
+              disabled={disableAllInputs}
+              type="text"
+              placeholder="Surname"
+              style={{ color: inputsColor }}
+            />
+            <div className="btnDIV">
+              <button>Submit</button>
             </div>
-        </div>
-    )
-}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default RegistrationPage;
