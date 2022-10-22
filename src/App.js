5e5848303499ad934023ae36699659b7e612d20c
Boris <javadyan.boris@bk.ru>
2022-10-22 16:46:40 +0400

    start-react-tas-4-Boris

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ import RegistrationPage from "./components/Functional/RegistrationPage/Registrat
 import CalcVolume from "./components/calcVolume";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state={
-            width:0,
-            length:0
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      length: 0,
+    };
+  }
 
-    render() {
-        const{width,length}=this.state
-        return (
-             <Layout>
-                <input type='text' onChange={e=>this.setState({width:e.target.value})} placeholder="Width"/>
-                <input type='text' onChange={e=>this.setState({length:e.target.value})}  placeholder="length"/>
-                <h2>Area:{width*length}</h2>
-                <CalcVolume area={width*length} width={width} length={length}/>
-             </Layout>
-
-            
-           
-        );
-    }
+  render() {
+    const { width, length } = this.state;
+    return (
+      <Layout>
+        <input
+          type="text"
+          onChange={(e) => this.setState({ width: e.target.value })}
+          placeholder="Width"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.setState({ length: e.target.value })}
+          placeholder="length"
+        />
+        <h2>Area:{width * length}</h2>
+        <CalcVolume area={width * length} width={width} length={length} />
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/components/calcVolume/index.js
+++ src/components/calcVolume/index.js
@@ -1,15 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-
-export default function CalcVolume({area,width,length}){
-const [volume,setVolume]=useState('')
-    return(<>
-    <h3>height</h3>
-<input type='text' placeholder='Height' onChange={e=>setVolume(e.target.value*area)}/>
-<div>Volume:{width!==undefined&&length!==undefined? volume:''}</div>
-
-</>)
-
-
-
+export default function CalcVolume({ area, width, length }) {
+  const [volume, setVolume] = useState("");
+  return (
+    <>
+      <h3>height</h3>
+      <input
+        type="text"
+        placeholder="Height"
+        onChange={(e) => setVolume(e.target.value * area)}
+      />
+      <div>
+        Volume:{width !== undefined && length !== undefined ? volume : ""}
+      </div>
+    </>
+  );
 }
