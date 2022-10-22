d6c44d8107a7652d2f0c9fb3c7314e463d50a3a4
Suren <sur_zaqaryan@mail.ru>
2022-10-22 15:24:46 +0400

    fix 2

--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 function Calc({ area }) {
-  const [height, setHeight] = useState(0);
+  const [volume, setVolume] = useState(0);
 
   return (
-    <div className="wrapper">
-      <label htmlFor="inp">Height</label>
-      <input id="inp" onChange={(e) => setHeight(e.target.value)} type="number" />
-      <div>Volume: {height * area}</div>
+    <div>
+      <label htmlFor="input">Height</label>
+      <input id="input" onChange={(e) => setVolume(e.target.value * area)} type="number" />
+      <div>Volume: {volume}</div>
     </div>
   );
 }
